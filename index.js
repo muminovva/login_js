f090eba9347457530a5073a8d93f9fa345b0260c
muminovva <muminovvaumida@gmail.com>
2023-03-30 14:30:14 +0500

    login

---
+++ index.js
@@ -0,0 +1,21 @@
+// let text = document.querySelector(`.text`);
+// let btn = document.querySelector(`.btn`);
+
+
+
+// btn.onclick = (e) => {
+//     e.preventDefault()
+//     postData
+// }
+// const postData = e =>{
+    
+// }
+// const posData = (e) => {
+//     let value = btn
+// }
+document.querySelector(".btn").addEventListener("click", function(){
+    document.querySelector(".text").style.display = "flex";
+})
+document.querySelector(".fa-solid").addEventListener("click", function(){
+    document.querySelector(".text").style.display = "none";
+})
